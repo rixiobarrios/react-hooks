709729681c8f17ffc39a67c7ada5a1a12e03be77
Daniel Johnson <balancerockmedia@gmail.com>
2018-10-03 20:33:13 -0400

    starting point

---
+++ src/components/shared/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => (
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+  </ul>
+);
+
+export default Nav;
